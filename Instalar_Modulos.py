3d2f56b4a09c8d8a168ea71045d1c3baaa76132b
costarodriguesagap <36197226+costarodriguesagap@users.noreply.github.com>
2018-02-07 13:10:39 +0000

    Add files via upload

---
+++ Instalar_Modulos.py
@@ -0,0 +1,51 @@
+import Utilitarios
+
+loop = True
+
+if __name__ == "__main__":
+    
+    while loop:
+        
+        res_instalar = 0
+        modulo = ""
+        print("----- Opcao 1 - Instalar Modulo ------------------")    
+        print("----- Opcao 2 - Actualizar Modulo ----------------")
+        print("----- Opcao 3 - Desinstalar Modulo ---------------")
+        print("----- Opcao 4 - Consultar Modulos Instalados -----")
+        print("----- Opcao 5 - Executar comando PIP (ADHOC) -----")
+        opcao_res = int(Utilitarios.get_input("\nInsira a sua Opcao: "))
+        
+        if opcao_res == 1:
+            modulo = str(Utilitarios.get_input("Indique o Modulo a Instalar: "))
+            res_instalar = int((Utilitarios.cmd_pip("pip show " + modulo, True)))
+            if res_instalar == 0:
+                print("Modulo já está Instalado")
+            else:
+                Utilitarios.cmd_pip("pip install --no-cache-dir " + modulo, True)
+
+        if opcao_res == 2:
+            modulo = str(Utilitarios.get_input("Indique o Modulo a Actualizar: "))
+            res_instalar = int((Utilitarios.cmd_pip("pip show " + modulo, True)))
+            if res_instalar == 1:
+                print("Modulo ainda não se encontra Instalado, Instale primeiro o modulo na Opcao 1")
+            else:
+                Utilitarios.cmd_pip("pip install " + modulo + " --upgrade", True)
+
+        if opcao_res == 3:
+            modulo = str(Utilitarios.get_input("Indique o Modulo a Desinstalar: "))
+            res_instalar = int((Utilitarios.cmd_pip("pip show " + modulo, True)))
+            if res_instalar == 1:
+                print("Modulo ainda não se encontra Instalado")
+            else:
+                Utilitarios.cmd_pip("pip uninstall -y " + modulo, True)
+
+        if opcao_res == 4:
+            Utilitarios.cmd_pip("pip list", True)
+
+        if opcao_res == 5:
+            Utilitarios.cmd_pip(str(Utilitarios.get_input("Insira o comando: ")), True)
+            
+        control = Utilitarios.get_input("Pretende Continuar?: ('S' - Sim ou 'N' - Não) \nR:")
+
+        if (control.upper() == "N"):
+            loop = False
